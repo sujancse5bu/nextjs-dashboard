04ca52266f3ae1ad09d5018f3d8bfb539bb10410
sujan-mridha-lyxa <sujan.mridha@lyxa.ai>
2026-03-14 20:18:40 +0600

    in progress

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
 import Link from 'next/link';
 import AcmeLogo from './ui/acme-logo';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -28,16 +29,20 @@ export default function Page() {
           </Link>
           <div
             className="relative w-4 h-4 bg-transparent border-l-15 border-r-15 border-b-26 border-l-transparent border-r-transparent border-b-black"
+          /><div
+            className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
           />
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}<Image
+          {/* Add Hero Images Here */}
+          <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
-          /><Image
+          />
+          <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,29 +1,26 @@
-import { LatestInvoice } from '@/app/lib/definitions';
-import { lusitana } from '@/app/ui/fonts';
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { fetchLatestInvoices } from "@/app/lib/data";
+import { LatestInvoice } from "@/app/lib/definitions";
+import { lusitana } from "@/app/ui/fonts";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+import Image from "next/image";
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
+                    "border-t": i !== 0,
                   },
                 )}
               >
